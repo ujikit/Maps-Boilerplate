4cd0beb9c0973cd5982442447246f71add176920
Dota 2 <fauzizaki15@gmail.com>
2021-01-15 10:33:55 +0700

    [Monitor Driver] Done

--- src/configs/Color.js
+++ src/configs/Color.js
@@ -1,3 +1,4 @@
 module.exports = {
 	MAIN_COLOR: `rgb(75, 147, 29)`,
+	BLACK_FONT: `rgb(69, 67, 67)`,
 }
--- src/screens/MonitorDriver.js
+++ src/screens/MonitorDriver.js
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
   Dimensions,
+  FlatList,
   Image,
+  Platform,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import {
-  Container,
-  Icon,
-} from 'native-base';
+import {Container, Icon} from 'native-base';
 import Geolocation from 'react-native-geolocation-service';
-import MapView, { Marker, Polyline } from "react-native-maps";
+import MapView, {Marker, Polyline} from 'react-native-maps';
 const polyline = require('@mapbox/polyline');
+import BottomSheet from 'reanimated-bottom-sheet';
 import {connect, useDispatch} from 'react-redux';
 
 // components
@@ -21,10 +21,12 @@ import Line from '../components/Line'
 import Loader from '../components/Loader'
 // configs
 import {GOOGLE_API_KEY} from '../configs'
-import {MAIN_COLOR} from '../configs/Color'
+import {BLACK_FONT, MAIN_COLOR} from '../configs/Color'
 // redux actions
 import {userCurrentLocationDispatch} from '../states/actions/global_all_action'
 import {currentLocationDispatch, destinationLocationDispatch} from '../states/actions/destination_all_action'
+
+const SECOND_COLOR = 'rgb(170, 166, 166)'
 
 function MonitorDriverScreen ({
   route,
@@ -38,11 +40,14 @@ function MonitorDriverScreen ({
   is_edit_reducer,
 }) {
   const dispatch = useDispatch();
+  const RefBottomSheetSuccessTransaction = useRef(null);
 
   let {current_location} = route.params;
 
   let [is_loading, setIsLoading] = useState(true);
+  let [is_hidden_current_and_destination_view, setIsHiddenCurrentAndDestinationView] = useState(false);
   // let [is_loading, setIsLoading] = useState(false);
+  let [init_snap_points, setInitSnapPoint] = useState(0);
   let [init_region, setInitRegion] = useState({
     latitude: user_current_location_reducer.latitude,
     longitude: user_current_location_reducer.longitude,
@@ -179,48 +184,152 @@ function MonitorDriverScreen ({
       .catch(error_direction => console.error('error_fetching_direction_maps', error_place))
   }
 
+  _handleOpenModalSuccessTransaction = () => {
+    setInitSnapPoint(140)
+    setIsHiddenCurrentAndDestinationView(true)
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+      RefBottomSheetSuccessTransaction.current.snapTo(2)
+    }, 2000)
+  }
+
+  renderHeader = () => (
+    <View style={{borderTopLeftRadius: 10, borderTopRightRadius: 10, backgroundColor: 'rgb(244, 244, 244)', borderBottomWidth: 1, borderBottomColor: '#eee'}}>
+      <View style={{alignSelf: 'center'}}>
+        <Icon type="Feather" name="minus" style={{ color: 'rgb(159, 159, 159)', fontSize: 35 }} />
+      </View>
+    </View>
+  )
+
+  renderContent = () => (
+    <View style={{height: '100%', backgroundColor: 'white', padding: 25}}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{width: 80}}>
+          <Image
+            resizeMode="stretch"
+            style={{width: 60, height: 60}}
+            source={require('../assets/images/icons/Gojek/success-transaction.png')}
+          />
+        </View>
+        <View style={{flex: 1}}>
+          <Text style={{fontWeight: 'bold', fontSize: 22, color: BLACK_FONT}}>Terima kasih!</Text>
+        </View>
+      </View>
+      <View style={{flexDirection: 'row', marginTop: 20}}>
+        <View style={{flex: 1, borderWidth: 1, borderColor: 'rgb(212, 211, 211)', height: 110}}>
+          <View style={{flex: 1, padding: 10}}>
+            <View style={{height: 30, justifyContent: 'center'}}>
+              <Text style={{fontWeight: 'bold', fontSize: 17, color: BLACK_FONT}}>Dibayar pakai</Text>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+              <Image
+                resizeMode="stretch"
+                style={{width: 40, height: 40}}
+                source={require('../assets/images/icons/LinkAja.png')}
+              />
+            <Text style={{fontWeight: 'bold', fontSize: 24, color: BLACK_FONT, paddingLeft: 12}}>{choose_ride_type_reducer.price}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={{flexDirection: 'row', marginTop: 20}}>
+        <View style={{flex: 1, borderWidth: 1, borderColor: 'rgb(212, 211, 211)', height: 90}}>
+          <View style={{flex: 1, padding: 10}}>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+              <View style={{justifyContent: 'center'}}>
+                <Text style={{fontWeight: 'bold', fontSize: 17, color: BLACK_FONT}}>Pendapatan</Text>
+              </View>
+              <View style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
+                <Text style={{alignItems: 'flex-end', fontWeight: 'bold', fontSize: 20, color: BLACK_FONT}}>{choose_ride_type_reducer.price}</Text>
+              </View>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+              <View style={{justifyContent: 'center'}}>
+                <Text style={{fontWeight: 'bold', fontSize: 17, color: BLACK_FONT}}>Poin</Text>
+              </View>
+              <View style={{flex: 1, justifyContent: 'center', alignItems: 'flex-end'}}>
+                <Text style={{alignItems: 'flex-end', fontWeight: 'bold', fontSize: 20, color: BLACK_FONT}}>+1</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={{marginTop: 20}}>
+        <View style={{flex: 1, alignItems: 'center', height: 90}}>
+          <Text style={{fontWeight: 'bold', fontSize: 17, color: SECOND_COLOR}}>GR120398109238</Text>
+        </View>
+      </View>
+      <View style={{marginTop: 40}}>
+        <View style={{flex: 1, alignItems: 'center', height: 90}}>
+          <Text style={{fontWeight: 'bold', fontSize: 22, color: BLACK_FONT}}>Bagaimana customer Anda?</Text>
+        </View>
+      </View>
+      <View style={{marginTop: 45}}>
+        <FlatList
+          horizontal
+          data={['Kasar', 'Jengkelin', 'Nyebelin', 'Oke lah', 'Baik bgt!']}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingVertical: 9}}
+          keyExtractor = { (item, index) => index.toString() }
+          renderItem={({ item }) => (
+            <View style={{width: 60, alignItems: 'center'}}>
+              <Icon type="FontAwesome" name="star" style={{ color: SECOND_COLOR, fontSize: 35 }} />
+              <View style={{width: 50, alignItems: 'center'}}>
+                <Text numberOfLines={1} style={{color: SECOND_COLOR}}>{item}</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    </View>
+  )
+
   return (
     <Container>
       <View style={{flex: 1}}>
         <Loader show={is_loading}/>
         {
-          current_location_reducer.place_id || destination_location_reducer.place_id ?
-            <View style={{width: '100%', paddingHorizontal: 15, paddingTop: 20, position: 'absolute'}}>
-              <View style={{width: '100%', height: 80, flexDirection: 'row', paddingHorizontal: 15, backgroundColor: 'white', padding: 15, borderRadius: 20, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 5}}>
-                <View style={{width: 35}}>
-                  <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Image
-                      resizeMode="stretch"
-                      style={{width: 20, height: 20}}
-                      source={require('../assets/images/icons/Gojek/current-location-green.png')}
+          is_hidden_current_and_destination_view ?
+            null
+          :
+            current_location_reducer.place_id || destination_location_reducer.place_id ?
+              <View style={{width: '100%', paddingHorizontal: 15, paddingTop: 20, position: 'absolute'}}>
+                <View style={{width: '100%', height: 80, flexDirection: 'row', paddingHorizontal: 15, backgroundColor: 'white', padding: 15, borderRadius: 20, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 5}}>
+                  <View style={{width: 35}}>
+                    <View style={{flex: 1, justifyContent: 'center'}}>
+                      <Image
+                        resizeMode="stretch"
+                        style={{width: 20, height: 20}}
+                        source={require('../assets/images/icons/Gojek/current-location-green.png')}
                       />
+                    </View>
+                    <View style={{flex: 1, justifyContent: 'center'}}>
+                      <View/>
+                    </View>
+                    <View style={{flex: 1, justifyContent: 'center'}}>
+                      <Image
+                        resizeMode="stretch"
+                        style={{width: 20, height: 20}}
+                        source={require('../assets/images/icons/Gojek/destination-orange.png')}
+                        />
+                    </View>
                   </View>
-                  <View style={{flex: 1, justifyContent: 'center'}}>
-                    <View/>
-                  </View>
-                  <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Image
-                      resizeMode="stretch"
-                      style={{width: 20, height: 20}}
-                      source={require('../assets/images/icons/Gojek/destination-orange.png')}
-                      />
-                  </View>
-                </View>
-                <View style={{flex: 1, justifyContent: 'center', paddingRight: 15}}>
-                  <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Text numberOfLines={1}>{current_location_reducer.name}</Text>
-                  </View>
-                  <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Line height={2} margin_left={0}/>
-                  </View>
-                  <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Text numberOfLines={1}>{destination_location_reducer.name}</Text>
+                  <View style={{flex: 1, justifyContent: 'center', paddingRight: 15}}>
+                    <View style={{flex: 1, justifyContent: 'center'}}>
+                      <Text numberOfLines={1}>{current_location_reducer.name}</Text>
+                    </View>
+                    <View style={{flex: 1, justifyContent: 'center'}}>
+                      <Line height={2} margin_left={0}/>
+                    </View>
+                    <View style={{flex: 1, justifyContent: 'center'}}>
+                      <Text numberOfLines={1}>{destination_location_reducer.name}</Text>
+                    </View>
                   </View>
                 </View>
               </View>
-            </View>
-          :
-            null
+            :
+              null
         }
         <View style={styles.container}>
           <MapView
@@ -312,8 +421,8 @@ function MonitorDriverScreen ({
               </TouchableOpacity>
             </View>
           </View>
-          <View style={{backgroundColor: 'white', height: 200, borderTopRightRadius: 25, borderTopLeftRadius: 25, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 15 }}>
-            <View style={{flexDirection: 'row', paddingHorizontal: 20, paddingTop: 30}}>
+          <View style={{backgroundColor: 'white', height: 220, borderTopRightRadius: 25, borderTopLeftRadius: 25, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 15 }}>
+            <View style={{flex: 1, flexDirection: 'row', paddingHorizontal: 20, paddingTop: 30}}>
               <View style={{flex: 1, paddingRight: 10}}>
                 <View>
                   <Text style={{fontWeight: 'bold', fontSize: 19}}>R6508HP</Text>
@@ -330,12 +439,12 @@ function MonitorDriverScreen ({
                 />
               </View>
             </View>
-            <View style={{flexDirection: 'row', paddingHorizontal: 20}}>
+            <View style={{flex: 1, flexDirection: 'row', paddingHorizontal: 20}}>
               <View style={{justifyContent: 'center'}}>
                 <Text style={{fontSize: 16, color: 'rgb(80, 79, 79)'}}>Samikin</Text>
               </View>
-              <View style={{justifyContent: 'center', backgroundColor: 'white', borderRadius: 15, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 5, marginLeft: 10}}>
-                <View style={{flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 7}}>
+              <View style={{justifyContent: 'center'}}>
+                <View style={{flexDirection: 'row', paddingHorizontal: 10, paddingVertical: 7, backgroundColor: 'white', borderRadius: 15, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.8, shadowRadius: 2, elevation: 3, marginLeft: 10}}>
                   <View style={{width: 30, justifyContent: 'center', alignItems: 'center'}}>
                     <Icon type="FontAwesome" name="trophy" style={{ color: 'rgb(244, 141, 6)', fontSize: 15 }} />
                   </View>
@@ -345,9 +454,29 @@ function MonitorDriverScreen ({
                 </View>
               </View>
             </View>
+            <View style={{flex: 1, flexDirection: 'row', paddingHorizontal: 20}}>
+              <View style={{width: 50, justifyContent: 'center'}}>
+                <View style={{width: 45, height: 45, justifyContent: 'center', alignItems: 'center', backgroundColor: MAIN_COLOR, borderRadius: 30}}>
+                  <Icon type="FontAwesome" name="phone" style={{ color: 'white', fontSize: 19 }} />
+                </View>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingLeft: 10}}>
+                <TouchableOpacity onPress={() => _handleOpenModalSuccessTransaction()} style={{flex: 1, backgroundColor: MAIN_COLOR, borderRadius: 30, height: 50}}>
+                  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', borderRadius: 30}}>
+                    <Text style={{fontSize: 19, color: 'white'}}>Perjalanan Selesai</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
         </View>
       </View>
+      <BottomSheet
+        ref={RefBottomSheetSuccessTransaction}
+        snapPoints={[init_snap_points, 140, '90%']}
+        renderHeader={renderHeader}
+        renderContent={renderContent}
+      />
     </Container>
   )
 }
